be1597279662ebd0986a40fcdcc3d72f2b5902b6
silent-x <silent.xx@gmail.com>
2021-11-15 11:11:22 +0800

    add acl_depends to hide menu showing without login

--- luasrc/controller/chinadns-ng.lua
+++ luasrc/controller/chinadns-ng.lua
@@ -4,5 +4,7 @@ function index()
 	if not nixio.fs.access("/etc/config/chinadns-ng") then
 		return
 	end
-	entry({"admin", "services", "chinadns-ng"}, cbi("chinadns-ng"), _("ChinaDNS-NG"), 70).dependent = true
+	page = entry({"admin", "services", "chinadns-ng"}, cbi("chinadns-ng"), _("ChinaDNS-NG"), 70)
+	page.dependent = true
+	page.acl_depends = { "luci-app-chinadns-ng" }
 end
